8109918297c2f8e5a41fd5b85c20c3f75607b6c2
Thomas Pollhammer <ic16b008@technikum-wien.at>
2017-03-21 18:14:57 +0100

    add project Task4

---
+++ tasks/Task4/Lesson4/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MusicCollection_Test
+{
+    [TestFixture]
+
+    public class Tests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void AddInvalidArtistName()
+        {
+            Assert.Catch(() =>
+            {
+                var a = new MusicCollection("", "testAlbum", 2000, 1, eFileType.FLAC);
+            });
+        }
+
+        [Test]
+        public void AddInvalidAlbumName()
+        {
+            Assert.Catch(() =>
+            {
+                var a = new MusicCollection("testArtist", "", 2000, 1, eFileType.FLAC);
+            });
+        }
+
+        [Test]
+        public void AddInvalidYear()
+        {
+            Assert.Catch(() =>
+            {
+                var a = new MusicCollection("testArtist", "testAlbum", 1837, 1, eFileType.FLAC);
+            });
+        }
+    }
+}
